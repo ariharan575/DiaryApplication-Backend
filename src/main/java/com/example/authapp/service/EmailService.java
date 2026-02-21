66a40f5439b451b5132884e7ce49844a8587ec42
ARIHARAN <fmoon6795@gmail.com>
2026-02-21 13:42:34 +0530

    update last commit

--- src/main/java/com/example/authapp/service/EmailService.java
+++ src/main/java/com/example/authapp/service/EmailService.java
@@ -1,28 +1,53 @@
 package com.example.authapp.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender javaMailSender;
+    @Value("${resend.api.key}")
+    private String apiKey;
 
-    public void sendOtpToEmail(String toEmail,String otp){
-        SimpleMailMessage sendOtp = new SimpleMailMessage();
-        sendOtp.setFrom("notesdaily19@gmail.com");
-        sendOtp.setTo(toEmail);
-        sendOtp.setSubject("OTP verification for Register");
-        sendOtp.setText("Hi,       " +
-                "Enter this code to continue logging in , code is valid for 5 minites ." +
-                " By entering this code, you will also confirm the email address associated with your account."  + "OTP: " + otp);
+    private final RestTemplate restTemplate;
 
-        javaMailSender.send(sendOtp);
+    public EmailService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
+    public void sendOtpToEmail(String toEmail, String otp) {
 
+        String url = "https://api.resend.com/emails";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(apiKey);
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("from", "onboarding@resend.dev"); // Free tier only
+        body.put("to", toEmail);
+        body.put("subject", "OTP Verification");
+        body.put("html", "<h2>Your OTP is: " + otp + "</h2>");
+
+        HttpEntity<Map<String, Object>> entity =
+                new HttpEntity<>(body, headers);
+
+        try {
+            ResponseEntity<String> response =
+                    restTemplate.postForEntity(url, entity, String.class);
+
+            System.out.println("Email sent successfully: " + response.getBody());
+
+        } catch (HttpClientErrorException e) {
+            System.err.println("Resend API Error: " + e.getResponseBodyAsString());
+        } catch (Exception e) {
+            System.err.println("Unexpected Error: " + e.getMessage());
+        }
+    }
 }
-
